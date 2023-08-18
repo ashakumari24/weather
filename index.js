2ca4f5f045ac3c57a448e67a5735c4d9da2c9eee
Asha Kumari <ap0525854@gmail.com>
2023-08-18 08:41:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '658fe28481msh474994bc84b4e6fp16f9ffjsn7ced73a52a0e',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather=(city)=>{
+    cityName.innerHTML=city
+
+    
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
+	.then(response => response.json())
+	.then((response) => {console.log(response)
+    
+        // cloud_pct.innerHTML=response.cloud_pct
+        temp.innerHTML=response.temp
+        temp2.innerHTML=response.temp
+        feels_like.innerHTML=response.feels_like
+        humidity.innerHTML=response.humidity
+        humidity2.innerHTML=response.humidity
+        min_temp.innerHTML=response.min_temp
+        max_temp.innerHTML=response.max_temp
+        wind_speed.innerHTML=response.wind_speed
+        wind_speed2.innerHTML=response.wind_speed
+        wind_degrees.innerHTML=response.wind_degrees
+        sunrise.innerHTML=response.sunrise
+        sunset.innerHTML=response.sunset
+
+
+    })
+	.catch(err => console.error(err));
+
+}
+
+submit.addEventListener("click",(e)=>{
+    e.preventDefault()
+    getWeather(city.value)
+})
+getWeather("Delhi")
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Patna', options)
+	.then(response => response.json())
+	.then((response) => {console.log(response)
+    
+        temp3.innerHTML=response.temp
+        humidity3.innerHTML=response.humidity
+        wind_speed3.innerHTML=response.wind_speed
+
+    
+    })
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Lucknow', options)
+	.then(response => response.json())
+	.then((response) => {console.log(response)
+    
+        temp4.innerHTML=response.temp
+        humidity4.innerHTML=response.humidity
+        wind_speed4.innerHTML=response.wind_speed
+
+    
+    })
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Kolkata', options)
+	.then(response => response.json())
+	.then((response) => {console.log(response)
+    
+        temp5.innerHTML=response.temp
+        humidity5.innerHTML=response.humidity
+        wind_speed5.innerHTML=response.wind_speed
+
+    
+    })
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Chennai', options)
+	.then(response => response.json())
+	.then((response) => {console.log(response)
+    
+        temp6.innerHTML=response.temp
+        humidity6.innerHTML=response.humidity
+        wind_speed6.innerHTML=response.wind_speed
+
+    
+    })
+        
+
